3619ce300de4592864eb0cb2a494761f9a83a4e3
Jaime Rodríguez <jaimerodriguezramos10@gmail.com>
2021-12-30 17:24:35 +0100

    Version @0.1

---
+++ main.js
@@ -0,0 +1,17 @@
+const typed = new Typed ('.subtitle-typed', {
+    strings: [
+        "a FrontEnd Developer.",
+        "a Coder.",
+        "a new programmer.",
+    ],
+	typeSpeed: 85,
+	startDelay: 300,
+	backSpeed: 75,
+	shuffle: false,
+	backDelay: 1500,
+	loop: true, 
+	loopCount: false, 
+	showCursor: true, 
+	cursorChar: '|',
+	contentType: 'html', 
+});
